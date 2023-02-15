be3505ea0158e6325372c6599910be8ba9319c70
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-02-15 02:18:51 -0600

    prune git

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 # build_files.sh
+git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
+git reflog expire --expire=now --all
+git gc --aggressive --prune=now
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
