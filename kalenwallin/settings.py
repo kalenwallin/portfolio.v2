6a48c783358f7e23847fd224a2160a7775627340
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-02-15 01:29:15 -0600

    Vercel build config

--- kalenwallin/settings.py
+++ kalenwallin/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel-app','django.kalenwallin.com']
+ALLOWED_HOSTS = ['.vercel-app','.now.sh','django.kalenwallin.com']
 
 
 # Application definition
