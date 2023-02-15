5eae37442d05cfc82f0e5ae6f4efb89201ff8cf5
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-02-15 02:21:08 -0600

    fix2

--- kalenwallin/settings.py
+++ kalenwallin/settings.py
@@ -145,5 +145,5 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = '/home/kalenwallin/kalenwallin/static'
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
