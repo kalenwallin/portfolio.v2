cc58b9bb5b90d9f43a72c9c3400886cb83ca42ea
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-02-16 23:49:06 -0600

    Removed xframeoptions

--- kalenwallin/settings.py
+++ kalenwallin/settings.py
@@ -55,10 +55,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-X_FRAME_OPTIONS = 'ALLOW-FROM=https://*.kalenwallin.com'
 
 ROOT_URLCONF = 'kalenwallin.urls'
 
