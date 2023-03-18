22c12d81b005f15f803b9b6d9c86d371f57aed21
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-03-18 04:09:00 -0500

    Removed hearting functionality

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.conf.urls.static import static
 from django.contrib import admin
 
 from . import views
@@ -8,5 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('<slug:slug>/', views.page.as_view(), name='page'),
-    path('like/<int:id>', views.page_heart, name='page_heart'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Page, Lover, Category
+from .models import Category, Article
 
 
 def index(request):
@@ -12,48 +12,12 @@ def index(request):
 def fappy_borbs(request):
     return render(request, 'portfolio/fappy_borbs/fappy_borbs.html')
 
-# Hearting code modified from here: https://www.youtube.com/watch?v=AZwc9hDBi04
-
-
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
-
 
 class page(generic.DetailView):
-    model = Page
+    model = Article
     template_name = 'portfolio/page.html'
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
-        ip = get_client_ip(self.request)
-        if Lover.objects.filter(ip=ip).exists():
-            context['ip'] = Lover.objects.filter(ip=ip).first()
         return context
-
-
-def page_heart(request, id):
-    if request.method == "POST":
-        instance = Page.objects.get(id=id)
-        ip = get_client_ip(request)
-        if not Lover.objects.filter(ip=ip).exists():
-            lover = Lover.objects.create(ip=ip)
-        else:
-            lover = Lover.objects.filter(ip=ip).first()
-
-        if not instance.hearts.filter(id=lover.id).exists():
-            instance.hearts.add(lover)
-            instance.save()
-            context = {'page': instance, 'ip': lover}
-            return render(request, 'portfolio/page_heart.html', context)
-        else:
-            instance.hearts.remove(lover)
-            instance.save()
-            context = {'page': instance, 'ip': lover}
-            return render(request, 'portfolio/page_heart.html', context)
