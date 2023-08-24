e3ec205dd6305a504d7e9776cfef7e21270f3a51
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-08-24 08:55:04 -0500

    Add vercel to allowed hosts

--- kalenwallin/settings.py
+++ kalenwallin/settings.py
@@ -14,7 +14,8 @@ import os
 import environ
 from pathlib import Path
 import socket
-socket.getaddrinfo('localhost', 8080)
+
+socket.getaddrinfo("localhost", 8080)
 
 env = environ.Env()
 environ.Env.read_env()
@@ -32,67 +33,74 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel-app', '.now.sh', 'v2.kalenwallin.com',
-                 'localhost', '*.kalenwallin.com', 'kalenwallin.com',
-                 'dj.kalenwallin.com',]
+ALLOWED_HOSTS = [
+    ".vercel-app",
+    ".now.sh",
+    "v2.kalenwallin.com",
+    "localhost",
+    "*.kalenwallin.com",
+    "kalenwallin.com",
+    "dj.kalenwallin.com",
+    "*.vercel.app",
+]
 
 # Markdownfield requirement
-SITE_URL = 'dj.kalenwallin.com'
+SITE_URL = "dj.kalenwallin.com"
 
 # Application definition
 
 INSTALLED_APPS = [
-    'portfolio.apps.PortfolioConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'markdownfield',
-    'autoslug',
+    "portfolio.apps.PortfolioConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "markdownfield",
+    "autoslug",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
 ]
 
-ROOT_URLCONF = 'kalenwallin.urls'
+ROOT_URLCONF = "kalenwallin.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'kalenwallin.wsgi.application'
+WSGI_APPLICATION = "kalenwallin.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.postgresql_psycopg2",
-        'HOST': env("DATABASE_HOST"),
-        'NAME': env("DATABASE_NAME"),
-        'USER': env("DATABASE_USER"),
-        'PASSWORD': env("DATABASE_PASSWORD"),
-        'PORT': env("DATABASE_PORT"),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": env("DATABASE_HOST"),
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "PORT": env("DATABASE_PORT"),
     }
     # 'default': {
     #    'ENGINE': 'django.db.backends.sqlite3',
@@ -106,20 +114,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': '''django.contrib.auth.password_validation
-        .UserAttributeSimilarityValidator''',
+        "NAME": """django.contrib.auth.password_validation
+        .UserAttributeSimilarityValidator""",
     },
     {
-        'NAME': '''django.contrib.auth.password_validation
-        .MinimumLengthValidator''',
+        "NAME": """django.contrib.auth.password_validation
+        .MinimumLengthValidator""",
     },
     {
-        'NAME': '''django.contrib.auth.password_validation
-        .CommonPasswordValidator''',
+        "NAME": """django.contrib.auth.password_validation
+        .CommonPasswordValidator""",
     },
     {
-        'NAME': '''django.contrib.auth.password_validation
-        .NumericPasswordValidator''',
+        "NAME": """django.contrib.auth.password_validation
+        .NumericPasswordValidator""",
     },
 ]
 
@@ -127,9 +135,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -140,7 +148,7 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Backblaze B2 Buckets based on AWS S3
 # AWS_ACCESS_KEY_ID = env("BACKBLAZE_ACCESS_KEY_ID")
@@ -153,10 +161,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 BACKBLAZE_URL = env("BACKBLAZE_URL")
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
-MEDIA_URL = BACKBLAZE_URL + '/media/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+MEDIA_URL = BACKBLAZE_URL + "/media/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
