8fe980e008648b6259a97226c45db6fecf2ec20d
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-08-24 09:09:13 -0500

    Added debug and production modes based on env

--- kalenwallin/settings.py
+++ kalenwallin/settings.py
@@ -31,21 +31,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = True if env("DEBUG") == "True" else False
 
 ALLOWED_HOSTS = [
     ".vercel-app",
     ".now.sh",
     "v2.kalenwallin.com",
     "localhost",
-    "*.kalenwallin.com",
+    ".kalenwallin.com",
     "kalenwallin.com",
     "dj.kalenwallin.com",
-    "*.vercel.app",
+    ".vercel.app",
 ]
 
 # Markdownfield requirement
-SITE_URL = "dj.kalenwallin.com"
+SITE_URL = "v2.kalenwallin.com"
 
 # Application definition
 
